f5a53f16dc8b60a4c5bb73d69ea15879702795ae
jootan19 <joo.tan@me.com>
2017-06-30 15:48:25 +0800

    style changes

--- bot.js
+++ bot.js
@@ -16,16 +16,39 @@ var params_jobs = {
 T.get('search/tweets', params_jobs, function(err, data, response) {
   if(!err){
     for(let i = 0; i < data.statuses.length; i++){
+      
       // -- GET USERNAME OF TWEET-ER
       var username = String(data.statuses[i].user.screen_name);
+      
+      // -- GET TWEET URL
       var tweeturl  = ': twitter.com/' + username + '/statuses/' + data.statuses[i].id_str;
-      var retweetbody = queryterm  +' has caught my attention\nMost recent tweet by @' + username + tweeturl;
+      
+      // -- FORMAT TWEET
+      var tweetfmtID = Math.floor(Math.random()*5);
+      switch(tweetfmtID){
+        case 0:
+          let retweetbody = queryterm  +' has caught my attention\nMost recent tweet by @' + username + tweeturl;  
+          break;
+        case 1:
+          let retweetbody = 'Who fancies a bit of '+  queryterm  +' ? \n@' + username + 'tweeted most recently about it' + tweeturl;  
+          break;
+        case 2:
+          let retweetbody = 'Thinking of '+  queryterm  +' now.\n@' + username + 'seems to be thinking of it too:' + tweeturl;  
+          break;
+        case 3:
+          let retweetbody = 'hmmm...pondering' + queryterm  +'...\nso is @' + username + tweeturl;  
+          break;
+        case 4:
+          let retweetbody = queryterm  +' is thought provoking no?\n @' + username + ' thought about it too' + tweeturl;  
+          break;
+      }
+      
       T.post('statuses/update',{status:retweetbody},function(err,data){console.log(data.text)});
       T.post('favorites/create', {id: data.statuses[i].id_str}, function(err, response){});
     }
     
   }else{
-    var sadtweetbody = 'I feel sick..*ugh*...someone pls get help from @gnafuy...' ;
+    var sadtweetbody = 'I feel sick..*ugh*...someone pls get help from my human @gnafuy...' ;
     T.post('statuses/update',{status:sadtweetbody},function(err,data){console.log(data.text)});
   }});
               
